1e2023dc3f5afd90cd131d3ce46fde4831e80557
roksanaj91 <44161825+roksanaj91@users.noreply.github.com>
2021-08-16 19:14:03 +0200

    version with all imgs

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ async function getWeather(){
         url: 'http://api.openweathermap.org/data/2.5/weather?units=metric&lat=51.759445&lon=19.457216&appid=',
         dataType: 'json',
         success: function(apiResponse) {
-          console.log(apiResponse)
           populateWeatherData(apiResponse);
         },
       });
@@ -19,13 +18,13 @@ async function getWeather(){
 async function getQuote(){
     await fetch("https://quotes.rest/qod?language=en").then(response => response.json())
     .then(response => { 
-      // console.log(response);
       populateQuote(response);})
     .catch(error => console.log(error));
 }
 
 function populateWeatherData(jsonObject){
-    const weatherCode = jsonObject.weather[0].id;
+    const weatherCode = jsonObject.weather[0].id;;
+
     sunrise = jsonObject.sys.sunrise;
     sunset = jsonObject.sys.sunset;
     dateTime = jsonObject.dt;
@@ -46,27 +45,69 @@ function populateWeatherData(jsonObject){
 function checkDataToSetPics(weatherCode){
     if(weatherCode == 800 && !isNightOrDay()) {
       $("#iconId").attr("src","imgs/clearSkyNight.png");
+      $("#mainContainer").css("backgroundImage","url(./imgs/bgClearSkyNight.jpeg");
+      $("#mainContainer").css("color","rgba(192,191,186,1)");
       return;
     }
     if(weatherCode == 801 && !isNightOrDay()) {
       $("#iconId").attr("src","imgs/fewCloudsNight.png");
+      $("#mainContainer").css("backgroundImage","url(./imgs/bgFewCloudsNight.jpeg");
+      $("#mainContainer").css("color","rgba(192,191,186,0.8)");
       return;
     }
 
-    if(weatherCode == 800) $("#iconId").attr("src","imgs/clearSky.png");
+    if(weatherCode == 800){
+       $("#iconId").attr("src","imgs/clearSky.png");
+       $("#mainContainer").css("backgroundImage","url(./imgs/bgClearSky.jpeg");
+       $("#mainContainer").css("color","rgba(10,10,15,0.8)");
+       $("#glassContainer").css("background","rgba(250, 250, 250, 0.4)")
+    }
 
-    if(weatherCode == 801) $("#iconId").attr("src","imgs/fewClouds.png");
-    if(weatherCode >= 802 && weatherCode <= 803) $("#iconId").attr("src","imgs/scatteredClouds.png")
-    if(weatherCode == 804) $("#iconId").attr("src","imgs/brokenClouds.png");
+    if(weatherCode == 801) {
+      $("#iconId").attr("src","imgs/fewClouds.png");
+      $("#mainContainer").css("backgroundImage","url(./imgs/bgFewClouds.jpeg");
+      $("#glassContainer").css("background","rgba(200, 200, 200, 0.5)")
+    }
+    if(weatherCode >= 802 && weatherCode <= 803) {
+      $("#iconId").attr("src","imgs/scatteredClouds.png")
+      $("#mainContainer").css("backgroundImage","url(./imgs/bgScatteredClouds.jpeg");
+      $("#glassContainer").css("background","rgba(150, 150, 150, 0.5)")
+    }
+    if(weatherCode == 804){
+       $("#iconId").attr("src","imgs/brokenClouds.png");
+       $("#mainContainer").css("backgroundImage","url(./imgs/bgBrokenClouds.jpg");
+       $("#mainContainer").css("color","rgba(182,181,176,1)");
+       $("#glassContainer").css("background","rgba(0, 0, 0, 0.5)")
+    }
 
-    if(weatherCode >= 701 && weatherCode <= 781) $("#iconId").attr("src","imgs/mist.png");
+    if(weatherCode >= 701 && weatherCode <= 781) {
+      $("#iconId").attr("src","imgs/mist.png");
+      $("#mainContainer").css("backgroundImage","url(./imgs/bgMist.jpeg");
+      $("#mainContainer").css("color","rgba(220,230,220,0.8)");
+    }
 
-    if(weatherCode >= 600 && weatherCode <= 622) $("#iconId").attr("src","imgs/snow.png");
+    if(weatherCode >= 600 && weatherCode <= 622) {
+      $("#iconId").attr("src","imgs/snow.png");
+      $("#mainContainer").css("backgroundImage","url(./imgs/bgSnow.jpeg");
+      $("#glassContainer").css("background","rgba(0, 0, 0, 0.5)")
+      $("#mainContainer").css("color","rgba(250,250,250,0.8)");
+    }
 
-    if(weatherCode >= 300 && weatherCode <= 501 ) $("#iconId").attr("src","imgs/rain.png");
-    if(weatherCode >= 502 && weatherCode <= 531) $("#iconId").attr("src","imgs/showerRain.png");
+    if(weatherCode >= 300 && weatherCode <= 501 ) {
+      $("#iconId").attr("src","imgs/rain.png");
+      $("#mainContainer").css("backgroundImage","url(./imgs/bgRain.jpg");
+      $("#mainContainer").css("color","rgba(163,170,176,1)");
+    }
+    if(weatherCode >= 502 && weatherCode <= 531) {
+      $("#iconId").attr("src","imgs/showerRain.png");
+      $("#mainContainer").css("backgroundImage","url(./imgs/bgShowerRain.jpg");
+    }
     
-    if(weatherCode >= 200 && weatherCode <= 232) $("#iconId").attr("src","imgs/thunderstorm.png");
+    if(weatherCode >= 200 && weatherCode <= 232) {
+      $("#iconId").attr("src","imgs/thunderstorm.png");
+      $("#mainContainer").css("backgroundImage","url(./imgs/bgThunderstorm.jpeg");
+      $("#mainContainer").css("color","rgba(200,200,200,0.8)");
+    }
 }
 
 function populateQuote(jsonObject){
